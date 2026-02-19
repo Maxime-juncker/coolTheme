341b8f0c7fa9942edd9ed3e193e76ee9204a844f
Maxime Juncker <maxime.junck@gmail.com>
2026-02-19 12:02:46 +0100

    colors

--- lua/coolTheme/palette.lua
+++ lua/coolTheme/palette.lua
@@ -2,10 +2,41 @@ local M = {}
 
 M.colors = {
 
-	bg = "#1e1e1e",
-	bg_dark = "#1e1e1e",
+	-- base
+	bg =		"#1e1e1e",
+	bg1 =		"#16181D",
+	bg2 =		"#282C34",
 
-	fg = "#2ab1b8",
+	fg =		"#ABB2BF",
+	fg1 =		"#434955",
+	fg2 =		"#FBF1C7",
+	fg3 =		"#8b8b8b",
+
+	-- accent colors
+	red =		"#E06C75",
+	green =		"#689D6A",
+	yellow =	"#E5C07B",
+	orange =	"#E59B7B",
+	teal =		"#2ab1b8",
+	cyan =		"#2ab1b8",
+	blue =		"#2ab1b8",
+	purple =	"#C678DD",
+	pink =		"#DD78BB",
+	
+	-- UI
+	comment   = "#689D6A",
+	selection = "#45475a",
+	border    = "#585b70",
+	error     = "#E06C75",
+	warning   = "#FBF1C7",
+	info      = "#2ab1b8",
+	hint      = "#DD78BB",
+
+	-- Git
+	git_add    = "#689D6A",
+	git_change = "#2ab1b8",
+	git_delete = "#E06C75",
+	
 
 }
 
